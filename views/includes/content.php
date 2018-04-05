8eb782231665a95026a6903702ec31fe4c4a9257
Unknown <bdxran@hotmail.be>
2018-04-05 16:19:34 +0200

    update content
    Mise en place responsive du contenu

--- views/includes/content.php
+++ views/includes/content.php
@@ -3,44 +3,44 @@
     <div class="title_window">
       <h1>Jeux Pc</h1><a href="#" class="redirection_window">+</a>
     </div>
-    <ul id="pc">
-      <li class="pc"><a href="#" class="lien"><img src="../../css/image/farcry-5-pc.jpg" class="jacket"></a></li>
-      <li class="pc"><a href="#" class="lien"><img src="../../css/image/gta-5-pc.jpg" class="jacket"></a></li>
-      <li class="pc"><a href="#" class="lien"><img src="../../css/image/thewitcher-3-pc.jpg" class="jacket"></a></li>
-      <li class="pc"><a href="#" class="lien"><img src="../../css/image/assetocorsa-pc.jpg" class="jacket"></a></li>
+    <ul class="mise_page">
+      <li class="articleLeft"><a href="#" class="lien"><img src="../../css/image/farcry-5-pc.jpg" class="jacket"></a></li>
+      <li class="articleMiddle"><a href="#" class="lien"><img src="../../css/image/gta-5-pc.jpg" class="jacket"></a></li>
+      <li class="articleMiddle"><a href="#" class="lien"><img src="../../css/image/thewitcher-3-pc.jpg" class="jacket"></a></li>
+      <li class="articleRigth"><a href="#" class="lien"><img src="../../css/image/assetocorsa-pc.jpg" class="jacket"></a></li>
     </ul>
   </section>
   <section class="window">
     <div class="title_window">
       <h1>Jeux Ps4</h1><a href="#" class="redirection_window">+</a>
     </div>
-    <ul id="pc">
-      <li class="pc"><a href="#" class="lien"><img src="../../css/image/farcry-5-pc.jpg" class="jacket"></a></li>
-      <li class="pc"><a href="#" class="lien"><img src="../../css/image/gta-5-pc.jpg" class="jacket"></a></li>
-      <li class="pc"><a href="#" class="lien"><img src="../../css/image/thewitcher-3-pc.jpg" class="jacket"></a></li>
-      <li class="pc"><a href="#" class="lien"><img src="../../css/image/assetocorsa-pc.jpg" class="jacket"></a></li>
+    <ul class="mise_page">
+      <li class="articleLeft"><a href="#" class="lien"><img src="../../css/image/farcry-5-pc.jpg" class="jacket"></a></li>
+      <li class="articleMiddle"><a href="#" class="lien"><img src="../../css/image/gta-5-pc.jpg" class="jacket"></a></li>
+      <li class="articleMiddle"><a href="#" class="lien"><img src="../../css/image/thewitcher-3-pc.jpg" class="jacket"></a></li>
+      <li class="articleRigth"><a href="#" class="lien"><img src="../../css/image/assetocorsa-pc.jpg" class="jacket"></a></li>
     </ul>
   </section>
   <section class="window">
     <div class="title_window">
       <h1>Jeux Xbox</h1><a href="#" class="redirection_window">+</a>
     </div>
-    <ul id="pc">
-      <li class="pc"><a href="#" class="lien"><img src="../../css/image/farcry-5-pc.jpg" class="jacket"></a></li>
-      <li class="pc"><a href="#" class="lien"><img src="../../css/image/gta-5-pc.jpg" class="jacket"></a></li>
-      <li class="pc"><a href="#" class="lien"><img src="../../css/image/thewitcher-3-pc.jpg" class="jacket"></a></li>
-      <li class="pc"><a href="#" class="lien"><img src="../../css/image/assetocorsa-pc.jpg" class="jacket"></a></li>
+    <ul class="mise_page">
+      <li class="articleLeft"><a href="#" class="lien"><img src="../../css/image/farcry-5-pc.jpg" class="jacket"></a></li>
+      <li class="articleMiddle"><a href="#" class="lien"><img src="../../css/image/gta-5-pc.jpg" class="jacket"></a></li>
+      <li class="articleMiddle"><a href="#" class="lien"><img src="../../css/image/thewitcher-3-pc.jpg" class="jacket"></a></li>
+      <li class="articleRigth"><a href="#" class="lien"><img src="../../css/image/assetocorsa-pc.jpg" class="jacket"></a></li>
     </ul>
   </section>
   <section class="window">
     <div class="title_window">
       <h1>Jeux Switch</h1><a href="#" class="redirection_window">+</a>
     </div>
-    <ul id="pc">
-      <li class="pc"><a href="#" class="lien"><img src="../../css/image/farcry-5-pc.jpg" class="jacket"></a></li>
-      <li class="pc"><a href="#" class="lien"><img src="../../css/image/gta-5-pc.jpg" class="jacket"></a></li>
-      <li class="pc"><a href="#" class="lien"><img src="../../css/image/thewitcher-3-pc.jpg" class="jacket"></a></li>
-      <li class="pc"><a href="#" class="lien"><img src="../../css/image/assetocorsa-pc.jpg" class="jacket"></a></li>
+    <ul class="mise_page">
+      <li class="articleLeft"><a href="#" class="lien"><img src="../../css/image/farcry-5-pc.jpg" class="jacket"></a></li>
+      <li class="articleMiddle"><a href="#" class="lien"><img src="../../css/image/gta-5-pc.jpg" class="jacket"></a></li>
+      <li class="articleMiddle"><a href="#" class="lien"><img src="../../css/image/thewitcher-3-pc.jpg" class="jacket"></a></li>
+      <li class="articleRigth"><a href="#" class="lien"><img src="../../css/image/assetocorsa-pc.jpg" class="jacket"></a></li>
     </ul>
   </section>
 </div>
